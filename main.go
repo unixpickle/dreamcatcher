20f834e6087ec7b119fdaab87137786808446518
Alex Nichol <unixpickle@gmail.com>
2019-03-12 15:36:55 -0400

    drafted main routine

--- main.go
+++ main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"path"
 
+	"github.com/unixpickle/essentials"
+)
+
+func main() {
+	var filename string
+
+	flag.StringVar(&filename, "filename", "", "the name of the file to serve up")
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: dreamcatcher [flags] <endpoint URL>")
+		fmt.Fprintln(os.Stderr)
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		flag.Usage()
+		essentials.Die()
+	}
+
+	endpointStr := flag.Args()[0]
+	endpoint, err := url.Parse(endpointStr)
+	essentials.Must(err)
+
+	if filename == "" {
+		filename = path.Base(endpoint.Path)
+		// TODO: find basename from content disposition.
+	}
 }
